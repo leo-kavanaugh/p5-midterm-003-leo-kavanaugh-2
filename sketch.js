5b144e2de08c9eff6c8fd666dd5a5699961c6f76
Leo Kavanaugh <leokavanaugh@Leos-MacBook-Pro-3.local>
2025-03-25 18:52:39 -0400

    phase 4 complete

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
+var x;
+var changeDirection;
+var y;
+var changeDirection2;
+
 function setup() {
     createCanvas(500, 600);
     background(200);
+    changeDirection=false;
+    x = 210;
+    changeDirection2=false;
+    y = 275
 }
 
 function draw() {
@@ -13,15 +22,9 @@ function draw() {
     fill(255, 255, 255);
     circle(210, 150, 50);
     //tux left eye
-    fill(0, 0, 0)
-    circle(210, 150, 35);
-    //tux left pupil
     fill(255, 255, 255);
     circle(275, 150, 50);
     //tux left eye
-    fill(0, 0, 0);
-    circle(275, 150, 35);
-    //tux right pupil
     fill(255, 137, 0);
     ellipse(240, 190, 115, 60);
     //tux beak
@@ -37,4 +40,28 @@ function draw() {
     fill(0, 0, 0);
     ellipse(360, 380, 60, 220);
     //tux right arm
+
+    circle(x,150,35);
+    fill(0, 0, 0)
+    if(x>=220){
+        changeDirection=true}
+    else if (x<=200){
+        changeDirection=false}
+    if (x>=200 && changeDirection == false){
+        x=x+1}
+    else if(changeDirection == true){
+        x=x-1}
+    //left pupil
+
+    circle(y,150,35);
+    fill(0, 0, 0)
+    if(y>=285){
+        changeDirection2=true}
+    else if (y<=265){
+        changeDirection2=false}
+    if (y>=265 && changeDirection2 == false){
+        y=y+1}
+    else if(changeDirection2 == true){
+        y=y-1}
+    //right pupil
 }
